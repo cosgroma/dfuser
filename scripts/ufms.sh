d79c9dbf851027714cd281933a2ef546fc08d4fc
Mathew Cosgrove <mathew.cosgrove@ngc.com>
2024-10-19 13:47:11 -0700

    additional scripts

---
+++ scripts/ufms.sh
@@ -0,0 +1,9 @@
+# Description: Utility functions meant to clean-up and organize the current working directory where it runs.
+
+# This script will run python CLI meant to clean-up and organize the current working directory
+# where it runs.
+
+# It will have a user configuration file ~./.ufmsrc will contain configuration selections for the user to choose
+# things like workspace directory root path and style (flat or nested), file organization rules
+
+# It will check for the update status for ~/w
